4ef618e3c28440314923ac215990ec2b03952866
OrrinDrake <danbreaux@suddenlink.net>
2016-03-16 15:36:29 -0500

    Productivity yessss
    Main menu screen added with appropriate script. Script not fully
    implemented because I didnt touch build settings yet. Added Credits
    screen to be completed later.

---
+++ Ouija/Assets/Scripts/UI/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayHuman()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void PlayGhost()
+    {
+        SceneManager.LoadScene("GhostGame");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+	
+}
