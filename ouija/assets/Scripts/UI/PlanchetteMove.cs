fa1079f9e77ab285a7714ba402793095d0aca0a6
James Bellard <jbella5@lsu.edu>
2016-04-18 10:50:56 -0500

    added images to hud. added script so i can test it on my other computer

---
+++ ouija/assets/Scripts/UI/PlanchetteMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class PlanchetteMove : MonoBehaviour
+{
+    public float speed = 1;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = getLetterPosition();
+
+        float distanceToTarget = Mathf.Sqrt((target.x - transform.position.x) * (target.x - transform.position.x) + (target.y - transform.position.y) * (target.y - transform.position.y));
+        var dir = target - transform.position;
+
+
+
+
+
+
+        GetComponent<Rigidbody2D>().AddForce(dir * speed);
+    }
+
+    Vector3 getLetterPosition()
+    {
+        Vector3 target1 = new Vector3(21.0f, 203.0f, 0.0f);
+
+
+        return target1;
+    }
+
+
+
+
+}
