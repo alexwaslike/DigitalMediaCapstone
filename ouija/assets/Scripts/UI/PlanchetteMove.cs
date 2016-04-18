5364a5b1df1cd5508e1b7baf02dab7bf0350f406
James Bellard <jbella5@lsu.edu>
2016-04-18 12:47:15 -0500

    updated planchettemove.cs to work with all screenport sizes

--- ouija/assets/Scripts/UI/PlanchetteMove.cs
+++ ouija/assets/Scripts/UI/PlanchetteMove.cs
@@ -1,39 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-
+using UnityEngine.UI;
 
 public class PlanchetteMove : MonoBehaviour
 {
+    Vector3 target;
+    Vector3 startPos;
+    CanvasScaler scaler;
+
     public float speed = 1;
+
     // Use this for initialization
     void Start()
     {
-
+       startPos = transform.position;
+       target = startPos;
+       scaler = GetComponentInParent<CanvasScaler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 target = getLetterPosition();
+        
 
-        //float distanceToTarget = Mathf.Sqrt((target.x - transform.position.x) * (target.x - transform.position.x) + (target.y - transform.position.y) * (target.y - transform.position.y));
+        target = getLetterPosition();
+
         var dir = target - transform.position;
 
+        //Debug.Log("dir: "+dir + "/n");
 
 
 
-
-
+       // dir.x = dir.x * scaler.scaleFactor;
+       // dir.y = dir.y * scaler.scaleFactor;
         GetComponent<Rigidbody2D>().AddForce(dir * speed);
     }
-
-    Vector3 getLetterPosition()
+    //returns letter position relative to parent (UI board)
+   Vector3 getLetterPosition()
     {
-        Vector3 target1 = new Vector3(21.0f, 203.0f, 0.0f);
+        Vector3 letterPos = new Vector3(040.0f, -100.0f, 0.0f);
 
 
-        return target1;
+
+       letterPos.x = letterPos.x/1000 * scaler.scaleFactor * Screen.width;
+        letterPos.y = letterPos.y/1000 * scaler.scaleFactor *Screen.height;
+        return (startPos + letterPos);
     }
 
 
