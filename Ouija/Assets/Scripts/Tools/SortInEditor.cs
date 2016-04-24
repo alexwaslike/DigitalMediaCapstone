be6403f6a0f134714b49df90038aaf6c5d65d549
Alexandra Willis <awil294@lsu.edu>
2016-04-24 00:55:25 -0500

    Walls transparent when behind them

--- Ouija/Assets/Scripts/Tools/SortInEditor.cs
+++ Ouija/Assets/Scripts/Tools/SortInEditor.cs
@@ -5,13 +5,18 @@ public class SortInEditor : MonoBehaviour {
 
     private SpriteRenderer _renderer;
 
+    void Awake()
+    {
+        if (FindObjectOfType<GameController>() != null)
+            enabled = false;
+    }
+
 	void OnGUI()
     {
         if (_renderer == null)
             _renderer = GetComponent<SpriteRenderer>();
 
         _renderer.sortingOrder = 100 - Mathf.FloorToInt(transform.position.y * 4);
-
 
     }
 
---
+++ Ouija/Assets/Scripts/Tools/TestMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMovement : MonoBehaviour {
+
+    public float Speed = 1.0f;
+
+	void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontal, vertical, 0) * Speed * Time.deltaTime);
+    }
+}
