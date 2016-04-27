8ddbabf9a2d4a12c9dc599b14f1230641caa58c0
James Bellard <jbella5@lsu.edu>
2016-04-26 23:47:42 -0500

    got arduino working

---
+++ Ouija/Assets/Scripts/Controllers 1/ArduinoController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using UnityEngine.UI;
+
+public class ArduinoController : MonoBehaviour
+{
+    public OuijaInputHandler oih;
+    SerialPort sp;
+    string recieved;
+    void Start()
+    {
+        foreach (string str in SerialPort.GetPortNames())
+        {
+            Debug.Log(str);
+            if (str == "C0M4")
+            {
+
+            }
+        }
+        sp = new SerialPort("COM3", 9600);
+        sp.Open();
+        sp.ReadTimeout = 1;
+
+        
+    }
+
+    void Update()
+    {
+        try
+        {
+            recieved = sp.ReadLine();
+            Debug.Log(recieved);
+            oih.AddToText(recieved);
+            oih.SendBoardMessage(recieved);
+        }
+        catch (System.Exception)
+        {
+        }
+    }
+}
